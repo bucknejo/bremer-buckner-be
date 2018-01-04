9c03485c52c427af85cda354a2b0e0746fb7d194
jbuckner <jon@redbonesolutions.com>
2018-01-04 09:46:56 -0500

    Initialization

---
+++ src/main/java/com/github/bucknejo/bremerbuckner/BremerBucknerBeApplication.java
@@ -0,0 +1,12 @@
+package com.github.bucknejo.bremerbuckner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BremerBucknerBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BremerBucknerBeApplication.class, args);
+	}
+}
